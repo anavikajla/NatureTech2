de511a754407d2cbcf25dd9784e81394fc6efcb5
anavikajla <anavi1235@gmail.com>
2016-06-14 15:19:06 +0530

    KnowledgeCentre.java reworked so that data can be properly fetched from database to add to spinner and added all states.

--- app/src/main/java/com/example/android/naturetech/KnowledgeCentre.java
+++ app/src/main/java/com/example/android/naturetech/KnowledgeCentre.java
@@ -26,30 +26,27 @@ public class KnowledgeCentre extends Activity implements AdapterView.OnItemSelec
         locationDropdown.setOnItemSelectedListener(this);
 
 
-    /* Dropdown creation for vegetables */
 
-        List<String> vegetables = new ArrayList<>();
-        vegetables.add("Tomato");
-        vegetables.add("Potato");
-        vegetables.add("Cauliflower");
-        vegetables.add("Brinjal");
-        vegetables.add("Cucumber");
-        vegetables.add("Paprika");
-        vegetables.add("Bottle Gourd");
+    /* Dropdown creation for vegetables */
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> vegetables = databaseAccess.getVegetables();
+        databaseAccess.close();
 
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, vegetables);
 
+        // attaching data adapter to spinner
+        vegetableDropdown.setAdapter(dataAdapter);
+
         // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // attaching data adapter to spinner
-        vegetableDropdown.setAdapter(dataAdapter);
         vegetableDropdown.setPrompt("Select the vegetable you want to plant");
 
 
-        /* Dropdown creation for season */
 
+        /* Dropdown creation for season */
         List<String> season = new ArrayList<>();
         season.add("Summer");
         season.add("Winter");
@@ -64,27 +61,58 @@ public class KnowledgeCentre extends Activity implements AdapterView.OnItemSelec
 
         // attaching data adapter to spinner
         seasonDropdown.setAdapter(dataAdapter2);
-        seasonDropdown.setPrompt("Select the season you will plant in");
+        seasonDropdown.setPrompt("Select the season you will plant it in");
+
 
 
         /* Dropdown creation for location */
-
         List<String> location = new ArrayList<>();
+        location.add("Andaman and Nicobar Islands");
+        location.add("Andhra Pradesh");
+        location.add("Arunachal Pradesh");
+        location.add("Assam");
+        location.add("Bihar (BR)");
+        location.add("Chandigarh");
+        location.add("Chhattisgarh");
+        location.add("Dadra and Nagar Haveli");
+        location.add("Daman and Diu");
+        location.add("Goa");
+        location.add("Gujarat");
         location.add("Haryana");
-        location.add("New Delhi");
+        location.add("Himachal Pradesh");
+        location.add("Jammu and Kashmir");
+        location.add("Jharkhand");
+        location.add("Karnataka");
+        location.add("Kerala");
+        location.add("Lakshadweep");
         location.add("Madhya Pradesh");
+        location.add("Maharashtra");
+        location.add("Manipur");
+        location.add("Meghalaya");
+        location.add("Mizoram");
+        location.add("Nagaland");
+        location.add("National Capital Territory of Delhi");
+        location.add("Odisha");
+        location.add("Pondicherry");
         location.add("Punjab");
+        location.add("Rajasthan");
+        location.add("Sikkim");
+        location.add("Tamil Nadu");
+        location.add("Tripura");
+        location.add("Uttar Pradesh");
+        location.add("Uttarakhand");
+        location.add("West Bengal");
 
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, location);
 
+        // attaching data adapter to spinner
+        locationDropdown.setAdapter(dataAdapter3);
+
         // Drop down layout style - list view with radio button
         dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // attaching data adapter to spinner
-        locationDropdown.setAdapter(dataAdapter3);
         locationDropdown.setPrompt("Select your state");
-
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
